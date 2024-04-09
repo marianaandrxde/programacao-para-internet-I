726be13dad25cf9096507194e0564b50b6ed4f74
Mariana Andrade <mariana.olivandrade@gmail.com>
2024-04-09 11:00:29 -0300

    code restructuring

--- atividade03/js/script.js
+++ atividade03/js/script.js
@@ -8,27 +8,3 @@ function topo(){
         }
     )
 }
-
-//Validação de Login
-
-function login() {
-    var logado = 0;
-    var usuario = document.getElementById("usuario").value;
-    var senha = document.getElementById("senha").value;
-
-    if (usuario == "admin" && senha == "123456"){
-        window.location = "index.html";
-        logado = 1;
-
-    } else {
-        alert("Acesso negado! Dados incorretos.");
-    }
-
-}
-
-//Ativar alert no botão cadastrar
-
-function cadastro(){
-    alert("Cadastrado com sucesso!");
-    window.location.href = "index.html";
-}
